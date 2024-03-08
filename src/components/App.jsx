57b062faeadb6a43b558da1dfe50b8092f44cc59
Ayyoub-Kasmi <ayyoubkasmidz@gmail.com>
2024-03-08 02:55:15 +0100

    integrated leaderboard with Kaggle API

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,43 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios'
 import TopBar from './TopBar';
 import DisplayLeaderboard from './Leaderboard';
-import leaderboardData from '../leaderboardData';
+import constantLeaderboardData from '../leaderboardData';
 
 function App() {
-  return (
-    <>
-      <TopBar/>
-      <DisplayLeaderboard data={leaderboardData} /> 
-    </>
-  )
+    // const [leaderboardData, setLeaderboardData] = useState(constantLeaderboardData)
+    const [leaderboardData, setLeaderboardData] = useState([])
+    const [weights, setWeights] = useState([])
+
+    useEffect(() => {
+
+        const getLeaderboardData = async () => {
+            try {
+                // const response = await axios.get('http://localhost:5000/leaderboard-full')
+                const response = await axios.get('https://kaggle-leaderboard-scraping-flask.onrender.com/leaderboard-full')
+
+                console.log(response)
+                setLeaderboardData(response.data.infos)
+                setWeights(response.data.weights)
+            } catch (error) {
+                console.error(error)
+            }
+        }
+
+        getLeaderboardData()
+
+        const intervalId = setInterval(getLeaderboardData, 60000); // 60000 ms = 1 minute
+
+        // Clear the timeout if the component unmounts
+        return () => clearInterval(intervalId);
+    }, [])
+
+    return (
+        <>
+            <TopBar />
+            <DisplayLeaderboard data={leaderboardData} weights={weights} />
+        </>
+    )
 }
 
 export default App;
--- src/components/Leaderboard.jsx
+++ src/components/Leaderboard.jsx
@@ -1,50 +1,72 @@
-const LeaderboardRow = ({ rank, team, challenges }) => {
-    
-  return (
-      <tr>
-        <td>
-        {typeof rank === 'string' && rank.endsWith('.png') ? ( 
-          <img src={rank} alt={`Rank for ${team}`} />
-        ) : (
-          rank 
-        )}
-      </td>
-      <td>{team}</td>
-        {challenges.map((challenge, index) => (
-        <td key={index}>{challenge}</td> 
-      ))}
-      </tr>
-    );
-  };
-  
-  // Component to render the entire leaderboard
-  const DisplayLeaderboard = ({ data }) => { // Changed component name here
+const numberOfChallenges = 5;
+const challenges = [
+    "Challenge 01",
+    "Challenge 02",
+    "Challenge 03",
+    "Challenge 04",
+    "Challenge 05",
+    "Challenge 06",
+    "Challenge 07",
+    "Challenge 08",
+]
+
+const LeaderboardRow = ({ rank, team, scores }) => {
     return (
-      <div className="leaderboard-container">
-        <table>
-          <thead>
-            <tr>
-              <th>Rank</th>
-              <th>Team</th>
-              <th>Challenge 01</th>
-              <th>Challenge 02</th>
-              <th>Challenge 03</th>
-              <th>Challenge 04</th>
-              <th>Challenge 05</th>
-              <th>Challenge 06</th>
-              <th>Challenge 07</th>
-              <th>Challenge 08</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.map((entry, index) => (
-              <LeaderboardRow key={index} {...entry} />
-            ))}
-          </tbody>
-        </table>
-      </div>
+        <tr>
+            <td>
+                {rank === 1 && <img src="/award.png" alt="1st" />}
+                {rank === 2 && <img src="/award2.png" alt="2nd" />}
+                {rank === 3 && <img src="/award3.png" alt="3rd" />}
+                {rank > 3 && <p className="rank">{rank}</p>}
+            </td>
+
+            <td>{team}</td>
+
+            {
+                scores.map((score, index) => (
+                    <td key={index}>{score}</td>
+                ))
+            }
+
+        </tr>
     );
-  };
-  
-  export default DisplayLeaderboard; // Changed export name here
-  
+};
+
+// Component to render the entire leaderboard
+const DisplayLeaderboard = ({ data, weights }) => { // Changed component name here
+    console.log('DisplayLeaderboard', data)
+    return (
+        <div className="leaderboard-container">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Rank</th>
+                        <th>Team</th>
+                        {
+                            challenges.slice(0, numberOfChallenges).map(challenge => <th>{challenge}</th>)
+                        }
+                    </tr>
+                </thead>
+                <tbody>
+                    {data
+                        .sort((a, b) => {
+                            let aScore = 0;
+                            let bScore = 0;
+                            for (let i = 0; i < numberOfChallenges; i++) {
+                                aScore += a.scores[i] * weights[i];
+                                bScore += b.scores[i] * weights[i];
+                            }
+                            return bScore - aScore;
+                        })
+                        .map((entry, index) => {
+                            entry.rank = index + 1;
+                        return <LeaderboardRow key={entry.team} {...entry} />
+                    })}
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default DisplayLeaderboard; // Changed export name here
+
