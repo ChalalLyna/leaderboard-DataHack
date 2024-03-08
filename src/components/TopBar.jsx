4ffb4f71d239914275a2741eed479bd34571b2eb
Ayyoub-Kasmi <ayyoubkasmidz@gmail.com>
2024-03-08 03:04:25 +0100

    fixed top bar logo

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,13 +1,13 @@
 import '../index.css';
 
-export default function TopBar(){
-  return (
-    <div className="TopBar-container">
-      <div>
-        <img style={{ width: '200px', height :'50px' }} src='../../public/Frame212.png'></img>
-      </div>
-        <h1 className='top10'>Top 10</h1>
-    </div>
-  );
+export default function TopBar() {
+    return (
+        <div className="TopBar-container">
+            <div>
+                <img style={{ width: '200px', height: '50px' }} src='/Frame212.png'></img>
+            </div>
+            <h1 className='top10'>Top 10</h1>
+        </div>
+    );
 }
 
